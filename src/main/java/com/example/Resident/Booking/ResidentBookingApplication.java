d2ea0920c7c2f3bd0d1d222572c35583f0edc9a9
ManuRoy <manuroy62@gmail.com>
2025-11-15 00:06:09 +0530

    Initial commit-1

--- src/main/java/com/example/Resident/Booking/ResidentBookingApplication.java
+++ src/main/java/com/example/Resident/Booking/ResidentBookingApplication.java
@@ -1,6 +1,5 @@
 package com.example.Resident.Booking;
 
-#manu
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
