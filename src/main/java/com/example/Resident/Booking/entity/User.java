fb3d9fabe20ebe544abb5b8eb0128243e437a72e
ManuRoy <manuroy62@gmail.com>
2025-11-15 00:24:46 +0530

    Initial commit-1

--- src/main/java/com/example/Resident/Booking/entity/User.java
+++ src/main/java/com/example/Resident/Booking/entity/User.java
@@ -1,4 +1,44 @@
 package com.example.Resident.Booking.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false,unique = true)
+    private String phone;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
 }
