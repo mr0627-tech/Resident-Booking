7b0b48d833235b7f30d15858cad6661459b76aa3
ManuRoy <manuroy62@gmail.com>
2025-11-14 23:48:19 +0530

    Initial commit-1

---
+++ src/main/java/com/example/Resident/Booking/ResidentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Resident.Booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidentBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Resident/Booking/ResidentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resident.Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
